3691b0c28451889142d4b8528617f39d024240a5
harsh tapadia <harshtapadia11@gmail.com>
2026-02-08 14:57:35 +0530

    this is the second code

---
+++ Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n ; 
+    scanf("%d", &n) ;
+    int arr[n];
+
+    for( int i = 0; i< n; i++) {
+        scanf("%d", &arr[i]);
+
+    }
+    int target;
+    scanf("%d", &target);
+
+    for ( int i = 0 ; i<n; i++){
+        for(int j = i + 1; j<n; j++){
+            int a = arr[i] + arr[j]; 
+            if ( a == target){
+                printf("[%d , %d]", arr[i], arr[j]);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
