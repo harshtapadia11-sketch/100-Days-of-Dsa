5c8f4b9a7ceaeaf87e787803d3bc48203092bd8d
harshtapadia11-sketch <harshtapadia11@gmail.com>
2026-02-16 23:57:21 +0530

    Correct nested loop increments and calculate diagonal sum
    Fix the nested loop increment syntax and add sum calculation for diagonal elements.

--- Q28.c
+++ Q28.c
@@ -14,3 +14,35 @@
 // 4 5 6
 // 7 8 9
 
+# include <stdio.h> 
+
+int main() {
+    int arr[100][100]; 
+    int m, n; 
+    scanf("%d %d", &m, &n );
+
+    for ( int i = 0 ; i < m ; i++) {
+        for(int j = 0 ; j<n; j++) {
+            scanf("%d", &arr[i][j]); 
+        }
+        printf("\n"); 
+    }
+    for ( int i = 0 ; i < m ; i++) {
+        for(int j = 0 ; j<n; j++) {
+            printf("%d", arr[i][j]); 
+        }
+        printf("\n"); 
+    }
+    int sum = 0 ; 
+    for ( int i = 0 ; i < m ; i++) {
+        for(int j = 0 ; j<n; j++) {
+             if ( i == j ) {
+                sum += arr[i][j]; 
+             }
+        }
+        
+    }
+    printf("sum = %d", sum ) ; 
+    
+}
+  
